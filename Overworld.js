c2f1bcbcadf93248dff7a44573ab4cc9c481a405
arahmaninov <arahmaninov@yandex.ru>
2022-04-27 15:46:24 +0300

    feat: add game loop

--- Overworld.js
+++ Overworld.js
@@ -3,68 +3,39 @@ class Overworld {
 		this.element = config.element;
 		this.canvas = this.element.querySelector(".game-canvas");
 		this.ctx = this.canvas.getContext("2d");
+		this.map = null;
+	}
+
+	startGameLoop() {
+		const step = () => {
+
+			// Clear off the canvas
+			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+			// Draw Lower Layer
+			this.map.drawLowerImage(this.ctx);
+
+			// Draw Game Objects
+			Object.values(this.map.gameObjects).forEach(object => {
+				object.x += 0.02;
+				object.sprite.draw(this.ctx);
+			})
+
+			// Draw Upper Layer
+			this.map.drawUpperImage(this.ctx);
+
+			requestAnimationFrame(() => { // Browser feature
+				step();
+			})
+		}
+		step();
 	}
 
 	init() {
-		const image = new Image();
-		image.onload = () => {
-			this.ctx.drawImage(image, 0, 0)
-		};
-		image.src="images/maps/DemoLower.png";
+		this.map = new OverworldMap(window.OverworldMaps.Kitchen);
 
-		// Place Game Objects
-		const hero = new GameObject({
-			x: 5,
-			y: 6,
-		})
-		const npc1 = new GameObject({
-			x: 7,
-			y: 9,
-			src: "images/characters/npc1.png"
+		this.startGameLoop();
 
-		})
-
-		setTimeout(() => {
-			hero.sprite.draw(this.ctx);
-			npc1.sprite.draw(this.ctx);
-		}, 100)
 		
-
-		/*
-		const x = 5;
-		const y = 6;
-
-		const shadow = new Image();
-		shadow.onload = () => {
-			this.ctx.drawImage(
-				shadow, 
-				0, //left cut
-				0, //top cut
-				32, //width of cut
-				32, //height of cut
-				x * 16 - 8,
-				y * 16 - 18,
-				32,
-				32
-				)
-		}
-		shadow.src="images/characters/shadow.png";
-
-		const hero = new Image();
-		hero.onload = () => {
-			this.ctx.drawImage(
-				hero, 
-				0, //left cut
-				0, //top cut
-				32, //width of cut
-				32, //height of cut
-				x * 16 - 8,
-				y * 16 - 18,
-				32,
-				32
-				)
-		}
-		hero.src="images/characters/hero.png";
-		*/
 	}
 }
